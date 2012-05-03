f07151cadb9e8043e29c34f0fd95519e8abcfb1e
Tikhon Jelvis <tikhon@berkeley.edu>
2012-05-03 02:53:38 -0700

    Finished the tag function which does scope analysis. I haven't tested it, but I think it might work. Or maybe not.

--- src/Cow/Scope.hs
+++ src/Cow/Scope.hs
@@ -1,8 +1,8 @@
-module Cow.Scope where
+module Cow.Scope (tag) where
 
-import Control.Monad.State (State, get, put)
+import Control.Applicative ((<$>), (<$), (<*>), (<*), (*>))
+import Control.Monad.State (State, get, put, runState)
 
-import Data.Functor        ((<$>), (<$))
 import Data.Maybe          (mapMaybe, listToMaybe)
 
 import Cow.Type
@@ -18,16 +18,18 @@ instance Functor Tagged where fmap fn (Tagged i a) = Tagged i $ fn a
 type WithScopes a = State (Scopes a)
 
 class Scopable a where
-  bindings :: a -> [a]  -- For things that introduce bindings into the local scope.
+  bindings :: AST a -> [a]  -- For things that introduce bindings into the local scope.
   newEnv   :: a -> Bool -- For things that introduce a new scope.
   bound    :: a -> Bool -- For things that can be bound (e.g. variable names)
   
-getTag :: Eq a => [[(a, Tag)]] -> a -> Maybe Tag
-getTag scopes val = listToMaybe $ mapMaybe (lookup val) scopes
+getTag :: Eq a => a -> Scopes a -> Maybe Tag
+getTag val (Scopes _ scopes) = listToMaybe $ mapMaybe (lookup val) scopes
 
-pushScope :: [(a, Tag)] -> WithScopes a ()
-pushScope scope = get >>= put . (push scope)
-  where push val (Scopes lastTag scopes) = Scopes lastTag $ val:scopes
+pushScope :: WithScopes a ()
+pushScope = get >>= put . \ (Scopes lastTag scopes) -> Scopes lastTag ([]:scopes)
+
+popScope :: WithScopes a ()
+popScope = get >>= put . \ (Scopes lastTag scopes) -> Scopes lastTag (drop 1 scopes)
         
 increment :: WithScopes a ()
 increment = get >>= put . \ (Scopes lastTag scopes) -> Scopes (succ lastTag) scopes
@@ -36,8 +38,19 @@ bind :: a -> WithScopes a ()
 bind val = do Scopes lastTag (curr:rest) <- get
               put . Scopes (succ lastTag) $ ((val, lastTag):curr):rest
 
-tag :: Eq a => a -> WithScopes a (Tagged a)
-tag val = do Scopes lastTag scopes <- get
-             let newTag = Tagged lastTag val <$ increment
-             maybe newTag (return . (`Tagged` val)) $ getTag scopes val
-             
+newTag :: a -> WithScopes a (Tagged a)
+newTag val = do Scopes lastTag _ <- get
+                Tagged lastTag val <$ increment
+
+tagVal :: Eq a => a -> WithScopes a (Tagged a)
+tagVal val = getTag val <$> get >>= maybe (newTag val) (return . (`Tagged` val)) 
+                
+tag :: (Scopable a, Eq a) => AST a -> AST (Tagged a)
+tag val = fst . runState (go val) $ Scopes 0 [[]] 
+  where go ast@(Node value children)
+          | newEnv value = pushScope *> taggedNode <* popScope
+          | otherwise    = taggedNode
+          where taggedNode  = newBindings *> (Node <$> currTag <*> mapM go children)
+                newBindings = mapM_ bind (bindings ast)
+                currTag | bound value = tagVal value
+                        | otherwise   = newTag value
