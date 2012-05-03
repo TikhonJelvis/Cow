6ec17f48f7a84a7268b909adc4553dd74103582a
Tikhon Jelvis <tikhon@berkeley.edu>
2012-05-03 01:05:50 -0700

    Added a simple JavaScript program for testing the parser.

---
+++ src/test.js
@@ -0,0 +1,44 @@
+var a = 10,
+    b,
+    c;
+
+function blarg(a, b, c) {
+  var d = a + b * c
+  
+  return a + d * c + a
+}
+
+do {
+  a -= 2
+  console.log("The value is: " + a, "Blarg")
+  a += 10
+  a -= 11
+} while (a > 0);
+
+(function (a, b) {
+  return a + b;
+})(1, 2)
+
+var obj = {
+  hello : "world",
+  "complicated" : 42,
+  nested : {
+    a : 10,
+    b : 11,
+    empty : {},
+    method : function (a, b) {
+      while (a > b) {
+        --a;
+        b++;
+        console.log(a, b);
+        if (typeof a == typeof b) console.log("The same!")
+      }
+
+      return {
+        a : a,
+        b : b
+      };
+    }
+  }
+};
+  
