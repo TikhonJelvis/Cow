bf4d64fd3f79f57059e3735be025b9368874838e
Tikhon Jelvis <tikhon@berkeley.edu>
2012-05-09 23:03:34 -0700

    Fixed the substructure mapping. As far as I can tell, it works now.

--- src/Cow/Substructure.hs
+++ src/Cow/Substructure.hs
@@ -4,14 +4,49 @@ import Control.Applicative    ((<$>), (<*>))
 
 import Data.Algorithm.Munkres (hungarianMethodDouble)
 import Data.Array.Unboxed
+import Data.Either            (either)
 import Data.Function          (on)
+import Data.List              (sortBy)
 import Data.List.Extras.Argmax
 import Data.Maybe             (listToMaybe, mapMaybe, fromJust)
 
 import Cow.Diff
 import Cow.Type
-  
-match :: Eq a => AST a -> AST a -> [(AST a, AST a)]
+
+type Matching a = (AST a, AST a)
+
+propagate :: (a -> b) -> Either a a -> Either b b
+propagate fn (Left v) = Left $ fn v
+propagate fn (Right v) = Right $ fn v
+
+tagDiff :: (Show a, Eq a) => AST a -> AST a -> Diff a
+tagDiff left right = foldr combine (diff left right) . zip [1..] $ matches
+  where matches = sortBy (compare `on` weighMatching) $ match left right
+        weighMatching (l, r) = subjectiveWeight $ diff l r
+        combine (i, matching) oldTree = tagMatching i matching oldTree
+                                        
+tagMatching :: (Show a, Eq a) => Tag -> Matching a -> Diff a -> Diff a
+tagMatching tagId (removed, added) diffTree
+  | otherwise = either id id $ tagRemoved diffTree >>= tagAdded
+  where toFrom (Del a) = (From tagId a)
+        toFrom x = x
+        toTo (Ins a) = (To tagId a)
+        toTo x = x
+        tagRemoved deleted@(Node (Del v) children) 
+          | (Del <$> removed) == deleted = Right . Node (From tagId v) $ map (toFrom <$>) children
+        tagRemoved (Node v children) = propagate (Node v) $ combine children (tagRemoved <$> children)
+        tagAdded inserted@(Node (Ins v) children)
+          | (Ins <$> added) == inserted = Right . Node (To tagId v) $ map (toTo <$>) children
+        tagAdded (Node v children) = propagate (Node v) $ combine children (tagAdded <$> children)
+        combine children result = case foldl merge ([], False) $ zip children result of
+            (newChildren, True)  -> Right newChildren
+            (newChildren, False) -> Left newChildren
+        merge (newChildren, found) (child, Right newChild)
+          | found     = (newChildren ++ [child], True)
+          | otherwise = (newChildren ++ [newChild], True)
+        merge (newChildren, found) (child, Left{}) = (newChildren ++ [child], found)
+
+match :: Eq a => AST a -> AST a -> [Matching a]
 match left right = map toPair . fst $ hungarianMethodDouble inputArray 
   where pairs = allPairs numLeft numRight
         numLeft  = number left
@@ -29,14 +64,18 @@ findAST i n@(Node (i', _) children)
   | i == i'    = Just n
   | otherwise = listToMaybe $ mapMaybe (findAST i) children
 
+subjectiveWeight :: Eq a => Diff a -> Double
+subjectiveWeight (Node Non{} children) = 1 + sum (subjectiveWeight <$> children)
+subjectiveWeight (Node _ children)     = 0 + sum (subjectiveWeight <$> children)
+
 weight :: Eq a => AST a -> AST a -> Double
-weight a b = weighDiff 0.5 $ diff a b 
+weight a b = weighDiff 0.1 $ diff a b
 
 number :: AST a -> AST (Int, a)
 number = snd . go 1
-  where go n (Node val [])       = (n + 1, Node (n, val) [])
+  where go n (Node val [])       = (succ n, Node (n, val) [])
         go n (Node val children) = (n', Node (n, val) children')
-          where (n', children') = foldl combine (n + 1, []) children
+          where (n', children') = foldl combine (succ n, []) children
                 combine (index, acc) child =
                   let (index', child') = go index child in (index', acc ++ [child'])
 
