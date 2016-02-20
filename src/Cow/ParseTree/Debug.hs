8ca050c43cb3f7481c49faad98a378a3f1018187
Tikhon Jelvis <tikhon@jelv.is>
2016-02-19 19:32:20 -0800

    Added handy functions for quickly building parse trees with ints at the leaves from string.

---
+++ src/Cow/ParseTree/Debug.hs
@@ -0,0 +1,35 @@
+module Cow.ParseTree.Debug where
+
+import           Cow.ParseTree
+
+import           Text.Parsec
+import           Text.Parsec.String
+
+-- | A helper function that reads trees of ints from a string. Each
+-- node is denoted with square brackets which contain its children,
+-- separated by spaces. Leaves are just numbers.
+--
+-- For example, [[1][2[3 4]]] parses to:
+--
+--                     o
+--                    / \
+--                   o   o
+--                  /   / \
+--                 1   2   o
+--                        / \
+--                       3   4
+--
+readTree :: String -> Either ParseError (Parse Int)
+readTree str = parse tree "<interactive>" str
+  where tree =  Leaf () <$> leaf
+            <|> Node () <$> node
+
+        leaf = read <$> many1 digit <* spaces
+        node = between (char '[' *> spaces) (char ']' *> spaces) (many tree)
+
+-- | An unsafe version of readTree where a bad parse results in a
+-- runtime error.
+readTree' :: String -> Parse Int
+readTree' str = case readTree str of
+  Left err  -> error $ show err
+  Right res -> res
